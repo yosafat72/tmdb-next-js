2a2114f821d48edab55ed1d5d8d5671a80712338
Yosafat Hermawan S <33362676+yosafat72@users.noreply.github.com>
2025-05-09 12:49:18 +0700

    update navbar dropdown

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,15 +16,15 @@ export default function Navbar() {
                                 aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-example">
                             <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
                                  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                 strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                 <line x1="3" x2="21" y1="6" y2="6"/>
                                 <line x1="3" x2="21" y1="12" y2="12"/>
                                 <line x1="3" x2="21" y1="18" y2="18"/>
                             </svg>
                             <svg className="hs-collapse-open:block hidden shrink-0 size-4"
                                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                 stroke-linejoin="round">
+                                 fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                 strokeLinejoin="round">
                                 <path d="M18 6 6 18"/>
                                 <path d="m6 6 12 12"/>
                             </svg>
@@ -45,6 +45,37 @@ export default function Navbar() {
                            href="#">Work</a>
                         <a className="font-medium text-gray-600 hover:text-gray-400 focus:outline-hidden focus:text-gray-400"
                            href="#">Blog</a>
+                        <div className="relative hs-dropdown">
+                            <button id="hs-navbar-example-dropdown" type="button"
+                                    className="hs-dropdown-toggle flex items-center w-full text-gray-600 hover:text-gray-400 focus:outline-hidden focus:text-gray-400 font-medium"
+                                    aria-haspopup="menu" aria-expanded="false" aria-label="Mega Menu">
+                                Dropdown
+                                <svg
+                                    className="hs-dropdown-open:-rotate-180 sm:hs-dropdown-open:rotate-0 duration-300 ms-1 shrink-0 size-4"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                    <path d="m6 9 6 6 6-6"/>
+                                </svg>
+                            </button>
+
+                            <div
+                                className="hs-dropdown-menu absolute top-full left-0 mt-2 transition-[opacity,margin] ease-in-out duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 z-10 bg-white sm:shadow-md rounded-lg p-1 space-y-1 border border-gray-200 hidden"
+                                role="menu" aria-orientation="vertical" aria-labelledby="hs-navbar-example-dropdown">
+                                <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
+                                   href="#">
+                                    About
+                                </a>
+                                <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
+                                   href="#">
+                                    Downloads
+                                </a>
+                                <a className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
+                                   href="#">
+                                    Team Account
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
